1b8daa6231204f05142bd1a1c84cb33f795989db
Nahid Anik <nahidjahan.a@utkorsho.org>
2024-01-30 17:14:05 +0600

    first_commit

---
+++ app/src/main/java/com/example/noteapp/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    large = RoundedCornerShape(0.dp),
+    medium = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(4.dp)
+)
---
+++ app/src/main/java/com/example/noteapp/ui/theme/Theme.kt
@@ -0,0 +1,29 @@
+package com.example.noteapp.ui.theme
+
+import Shapes
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+
+@Composable
+fun NoteAppTheme(
+    darkTheme: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = DarkColorScheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
